6c9d2525a9ca250754cc36f257e9337432848bd3
y7yaaaaa123 <92207127+y7yaaaaa123@users.noreply.github.com>
2022-10-03 01:38:01 +0300

    test3

--- pydownload/settings.py
+++ pydownload/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/static/test/'
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
 MEDIA_ROOT = BASE_DIR / 'static/test/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
