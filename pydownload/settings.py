d26c1bb7ef85432e08f37aa8fe41083748002c65
y7yaaaaa123 <92207127+y7yaaaaa123@users.noreply.github.com>
2022-10-03 02:36:46 +0300

    test9

--- pydownload/settings.py
+++ pydownload/settings.py
@@ -122,11 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/test1')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [BASE_DIR, 'static']
 MEDIA_URL = '/static/test/'
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
+
 MEDIA_ROOT = BASE_DIR / 'static/test/'
 
 
